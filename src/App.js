2814882a3c67b492982edd7f2645d3ce9006f1a7
Sahar <sahar_amoorezaie@yahoo.com>
2024-01-09 10:30:24 -0800

    Concept of Composition in React (Children Props)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './Components/ExpenseItem';
-
-const expences = [
-  { title: "Household Expense", amount: 25000, date: new Date(2023, 4, 27) },
-  { title: "Travel Expense", amount: 1300, date: new Date(2023, 4, 27) },
-  { title: "Education fees", amount: 5000, date: new Date(2023, 4, 27) }
-];
-
+import Expenses from "./Components/Expenses";
 function App() {
+  const expenses = [
+    { title: "Household Expense", amount: 25000, date: new Date(2023, 4, 27) },
+    { title: "Travel Expense", amount: 1300, date: new Date(2023, 5, 28) },
+    { title: "Education fees", amount: 5000, date: new Date(2023, 6, 29) },
+  ];
   return (
     <div>
       <h2>Hello Coders!</h2>
-      <ExpenseItem title={expences[0].title} amount={expences[0].amount} date={expences[0].date} />
-      <ExpenseItem title={expences[1].title} amount={expences[1].amount} date={expences[1].date} />
-      <ExpenseItem title={expences[2].title} amount={expences[2].amount} date={expences[2].date} />
+      <Expenses items={expenses}/>
     </div>
-  );
+  )
 }
-
 export default App;
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,37 +1,17 @@
-import './ExpenseItem.css'
-
-/*const expenseDate = new Date(2024,1,6);
-const expenseTitle =  "Household Expense";
-const expenseAmount = 25000;*/
-
-
-import ExpenseDate from "./ExpenseDate.js";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
-    {
-        /*const month = props.date.toLocaleString('en-US', {month: 'long'});
-        const year = props.date.getFullYear();
-        const day = props.date.toLocaleString('en-US', {day: '2-digit'});*/
-    }
-
-
-    return (
-        <div className='expense-item'>
-           {
-                /*<div className='expense-item__date'>
-                    <div>{month}</div>
-                    <div>{year}</div>
-                    <div>{day}</div>
-                    </div>*/
-            }
-            <ExpenseDate date = {props.date}/>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
-            </div>
-        </div>
-    );
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">₹{props.amount}</div>
+      </div>
+    </Card>
+  );
 }
-export default ExpenseItem;
 
+export default ExpenseItem;
