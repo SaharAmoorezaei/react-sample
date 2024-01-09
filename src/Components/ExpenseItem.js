8824378939fb0c8f149f356dd94ec6d259bea5d8
Sahar <sahar_amoorezaie@yahoo.com>
2024-01-08 22:54:33 -0800

    Adding JavaScript Logic to React Components

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -4,10 +4,22 @@ import './ExpenseItem.css'
 const expenseTitle =  "Household Expense";
 const expenseAmount = 25000;*/}
 
+
+
 function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    
+    
+
     return (
         <div className='expense-item'>
-            <div className='expense-item__date'>{props.date.toDateString()}</div>
+            <div className='expense-item__date'>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
