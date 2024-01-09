0ee363e74d4ff11ec58aa47959edb88932e7762b
Sahar <sahar_amoorezaie@yahoo.com>
2024-01-08 16:54:38 -0800

    Make React Component Reusable

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,20 +1,19 @@
 import './ExpenseItem.css'
 
-const expenseDate = new Date(2024,1,6);
+{/*const expenseDate = new Date(2024,1,6);
 const expenseTitle =  "Household Expense";
-const expenseAmount = 25000;
+const expenseAmount = 25000;*/}
 
-function ExpenseItem()
-{
+function ExpenseItem(props) {
     return (
-                <div className='expense-item'>
-                    <div className='expense-item__date'>{expenseDate.toDateString()}</div>
-                    <div className='expense-item__description'>
-                        <h2>{expenseTitle}</h2>
-                        <div className='expense-item__price'>{expenseAmount}</div>
-                    </div>
-                </div>
-           );
+        <div className='expense-item'>
+            <div className='expense-item__date'>{props.date.toDateString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
+            </div>
+        </div>
+    );
 }
 export default ExpenseItem;
 
