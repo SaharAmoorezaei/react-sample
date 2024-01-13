59291516fe24227837ce68d2883fd8f9c086fc8f
Sahar <sahar_amoorezaie@yahoo.com>
2024-01-13 11:08:54 -0800

    Listening to Events and Working with Event Handlers in React

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -2,6 +2,16 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.css";
 
+
+// function clickHandler()
+// {
+//   console.log('Clicked;!')
+// }
+
+const clickHandler = () => {
+  console.log('Clicked!');
+};
+
 function ExpenseItem(props) {
   return (
     <Card className="expense-item">
@@ -10,6 +20,8 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">₹{props.amount}</div>
       </div>
+      {/*<button onClick={()=>{console.log('Clicked!')}}>Change the Title</button>*/}
+      <button onClick={clickHandler}>Change the Title</button>
     </Card>
   );
 }
